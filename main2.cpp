b2322b8c0edba1fac8cae01251eaba505b85ffe1
mim <mim@hotmail.co.uk>
2024-08-11 16:50:28 +0100

    Event loop setup

--- main2.cpp
+++ main2.cpp
@@ -3,12 +3,38 @@
 
 void main()
 {
-    InitWindow(640, 480, "test");
+    int width = 640;
+    int height = 480;
+
+    InitWindow(width, height, "test");
+    
+    if (setupESCAPI() < 1)
+    {
+        TraceLog(LOG_ERROR, "Unable to init ESCAPI");
+        return;
+    }
+
+    // Setup capture buffer
+    SimpleCapParams capture;
+    capture.mWidth = width;
+    capture.mHeight = height;
+    capture.mTargetBuf = new int[width * height];
+
+    // Begin capture
+    initCapture(0, &capture);
+    doCapture(0);
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RED);
         EndDrawing();
+
+        // Poll webcam
+        if (isCaptureDone(0))
+        {
+            doCapture(0);
+        }
     }
     CloseWindow();
 }
