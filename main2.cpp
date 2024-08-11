574914f7fcb0c45e8a7fc46ba34bc03ce6cb3087
mim <mim@hotmail.co.uk>
2024-08-11 17:11:44 +0100

    Pixel format is wrong, but it kinda works

--- main2.cpp
+++ main2.cpp
@@ -6,6 +6,7 @@ void main()
     int width = 640;
     int height = 480;
 
+    SetTargetFPS(10);
     InitWindow(width, height, "test");
     
     if (setupESCAPI() < 1)
@@ -20,19 +21,30 @@ void main()
     capture.mHeight = height;
     capture.mTargetBuf = new int[width * height];
 
+    Image image;
+    image.data = capture.mTargetBuf;
+    image.width = width;
+    image.height = height;
+    image.format = PIXELFORMAT_UNCOMPRESSED_R8G8B8;
+    image.mipmaps = 1;
+
     // Begin capture
     initCapture(0, &capture);
     doCapture(0);
+
+    Texture2D texture = LoadTextureFromImage(image);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RED);
+        DrawTexture(texture, 0, 0, WHITE);
         EndDrawing();
 
         // Poll webcam
         if (isCaptureDone(0))
         {
+            UpdateTexture(texture, capture.mTargetBuf);
             doCapture(0);
         }
     }
