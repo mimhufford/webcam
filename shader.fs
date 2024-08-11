3003daaa508cd1a87335277129bc7a72cdfed930
mim <mim@hotmail.co.uk>
2024-08-11 22:01:08 +0100

    Offset image so it appears in centre of circle

--- shader.fs
+++ shader.fs
@@ -13,7 +13,16 @@ uniform float size;
 
 void main()
 {
-    vec4 texelColor = texture(texture0, fragTexCoord);
+    // Offset to the left so the image centre
+    // is in the centre of the leftmost square
+    // -----------------         -----------------
+    // |         :     |         |         :     |
+    // |       x :     |   =>    |    x    :     |
+    // |         :     |         |         :     |
+    // -----------------         -----------------
+    vec2 tex = fragTexCoord;
+    tex.x += (4.0/3.0 - 1.0) / 2.0;
+    vec4 texelColor = texture(texture0, tex);
 
     vec2 uv = vec2(gl_FragCoord.x, gl_FragCoord.y) / size;
     vec2 centre = vec2(0.5, 0.5);
