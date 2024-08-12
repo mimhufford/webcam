c20a416a095652cbce808f9ac48a597149c219bc
Mim Hufford <mim@hotmail.co.uk>
2024-08-12 10:30:57 +0100

    Delete old SDL version

--- build.bat
+++ build.bat
@@ -1,11 +1,4 @@
-cl main.cpp escapi.cpp ^
-   /nologo ^
-   /I include ^
-   /link ^
-   /LIBPATH:lib SDL2.lib SDL2main.lib shell32.lib user32.lib ^
-   /SUBSYSTEM:WINDOWS
-
-cl main2.cpp escapi.cpp ^
+cl webcam.cpp escapi.cpp ^
    /nologo ^
    /I raylib/include ^
    /O2 ^
